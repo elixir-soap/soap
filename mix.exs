bf8273459d80eb756dc5f024535dea3fa3331f0f
Petr Stepchenko <nitrinodev@gmail.com>
2017-12-21 17:23:01 +0300

    add ex_doc for library docs

--- mix.exs
+++ mix.exs
@@ -38,9 +38,11 @@ defmodule Soap.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # Code style
       {:credo, "~> 0.5", only: :dev},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+
+      # Docs
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
 end
