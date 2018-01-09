357778d2ace5976c9a61c85d2a21e0474b8aae23
sigmen <romchky1@gmail.com>
2018-01-09 13:44:24 +0300

    Added body constructor

--- lib/soap/request/options.ex
+++ lib/soap/request/options.ex
@@ -5,29 +5,56 @@ defmodule Soap.Request.Options do
   Documentation for Soap.Request.Options.
   """
 
-  def init_model(locals, params) do
+  def init_model(soap_action, params) do
     %Soap.Request.Options{
-      headers: base_headers(locals[:soap_action]),
-      body: prepare_body(params)
+      headers: prepare_headers(soap_action),
+      body: prepare_body(soap_action, params)
     }
   end
 
-
-  # generate("inCommonParms", [{"userID", "WSPB"}, {"branchNumber", "0000"}, {"externalSystemCode", "P2B0"}, {"externalUserCode", "WSPB"}])
-  # =>
-  # <?xml version="1.0" encoding="UTF-8"?>
-  # <inCommonParms>
-  #   <userID>WSPB</userID>
-  #   <branchNumber>0000</branchNumber>
-  #   <externalSystemCode>P2B0</externalSystemCode>
-  #   <externalUserCode>WSPB</externalUserCode>
-  # </inCommonParms>
-  defp prepare_body(params) do
-    params |> generate
+  defp prepare_headers(soap_action) do
+    nil
   end
 
-  defp base_headers(soap_action) do
-    [{"SOAPAction", to_string(soap_action)},
-     {"Content-Type", "text/xml;charset=UTF-8"}]
+  defp prepare_body(soap_action, params) do
+    params
+    |> construct_xml_request_body
+    |> Enum.map(&(Tuple.to_list(&1)))
+    |> List.foldl([], &(&1 ++ &2))
+    |> List.to_tuple
+    |> generate
   end
+
+  defp construct_xml_request_body(params) when is_map(params) do
+    params |> Map.to_list |> Enum.map(&(construct_xml_request_body(&1)))
+  end
+
+  defp construct_xml_request_body(params) when is_list(params) do
+    params |> Enum.map(&(construct_xml_request_body(&1)))
+  end
+
+  defp construct_xml_request_body(params) when is_tuple(params) do
+    tag_name = params |> Tuple.to_list |> List.first
+    params
+    |> Tuple.to_list
+    |> Enum.map(&(construct_xml_request_body(&1)))
+    |> insert_tag_parameters
+    |> List.to_tuple
+  end
+
+  defp construct_xml_request_body(params) when is_atom(params), do: params |> to_string
+
+  defp construct_xml_request_body(params) when is_binary(params), do: params
+
+  defp construct_xml_request_body(params) when is_number(params), do: params |> to_string
+
+  defp tag_parameters(tag_name), do: nil
+
+  defp insert_tag_parameters(params) when is_list(params) do
+    tag_name = params |> List.first
+
+    params |> List.insert_at(1, tag_parameters(tag_name))
+  end
+
+  defp insert_tag_parameters(params), do: params
 end
