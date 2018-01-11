7f503ba43faef5b0fdf4f206336186d5349302f9
Petr Stepchenko <nitrinodev@gmail.com>
2018-01-11 14:03:14 +0300

    add envelop element

--- lib/soap/request/params.ex
+++ lib/soap/request/params.ex
@@ -2,7 +2,16 @@ defmodule Soap.Request.Params do
   @moduledoc """
   Documentation for Soap.Request.Options.
   """
-  import XmlBuilder, only: [generate: 1]
+  import XmlBuilder, only: [generate: 1, element: 3]
+
+  @schema_types %{
+    "xmlns:xsd" => "http://www.w3.org/2001/XMLSchema",
+    "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance"
+  }
+  @soap_version_namespaces %{
+    "1" => "http://schemas.xmlsoap.org/soap/envelope/",
+    "1.2" => "http://www.w3.org/2003/05/soap-envelope"
+  }
 
   @doc """
   Headers generator by soap action and custom headers.
@@ -30,10 +39,11 @@ defmodule Soap.Request.Params do
       iex(2)> Soap.Request.Params.build_body(:get, %{inCommonParms: [{"userID", "WSPB"}]})
       "<inCommonParms>\n\t<userID>WSPB</userID>\n</inCommonParms>"
   """
-  @spec build_body(soap_action :: String.t() | atom(), params :: map()) :: String.t()
-  def build_body(soap_action, params) do
+  @spec build_body(wsdl :: map(), soap_action :: String.t() | atom(), params :: map()) :: String.t()
+  def build_body(wsdl, soap_action, params) do
     params
     |> construct_xml_request_body
+    |> add_envelop_wrapper(wsdl)
     |> Enum.map(&Tuple.to_list/1)
     |> List.foldl([], &(&1 ++ &2))
     |> List.to_tuple
@@ -81,4 +91,16 @@ defmodule Soap.Request.Params do
 
   @spec insert_tag_parameters(params :: any()) :: any()
   defp insert_tag_parameters(params), do: params
+
+
+  defp add_envelop_wrapper(body, wsdl) do
+    soap_version = soap_version() |> to_string
+    soap_version_attribute = %{"xmlns:#{env_namespace()}" => @soap_version_namespaces[soap_version]}
+    envelop_attributes = @schema_types |> Map.merge(soap_version_attribute) |> Map.merge(custom_namespaces())
+    [element(:"env:Envelope", envelop_attributes, body)]
+  end
+
+  defp soap_version, do: Application.fetch_env!(:soap, :globals)[:version]
+  defp env_namespace, do: Application.fetch_env!(:soap, :globals)[:env_namespace] || :env
+  defp custom_namespaces, do: Application.fetch_env!(:soap, :globals)[:custom_namespaces] || %{}
 end
