657e0db94428a9388dd124b578b0c618aa42e689
sigmen <romchky1@gmail.com>
2018-01-12 15:28:03 +0300

    A little refactoring of test support fixtures module

--- test/soap/request_test.exs
+++ test/soap/request_test.exs
@@ -6,8 +6,7 @@ defmodule Soap.RequestTest do
   alias Soap.Wsdl
 
   test "#call returns response body" do
-    wsdl_path   = Fixtures.get_file_path("wsdl/SendService.wsdl")
-    {_, wsdl}   = Wsdl.parse_from_file(wsdl_path)
+    {_, wsdl}   = Fixtures.get_file_path("wsdl/SendService.wsdl") |> Wsdl.parse_from_file
     soap_action = "sendMessage"
     params      = %{inCommonParms: [{"userID", "WSPB"}]}
     http_poison_result = {:ok, %HTTPoison.Response{status_code: 200, body: "Anything"}}
