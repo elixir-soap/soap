7cd1bb1944184f6b838cfa23ab9a4e1d3ff29aa0
sigmen <romchky1@gmail.com>
2018-01-12 17:54:01 +0300

    Soap test refactoring

--- test/soap_test.exs
+++ test/soap_test.exs
@@ -4,37 +4,34 @@ defmodule SoapTest do
   import Mock
   alias Soap.Wsdl
 
+  @soap_action "sendMessage"
+  @request_params %{inCommonParms: [{"userID", "WSPB"}]}
+
   test "#call was success" do
     {_, wsdl}   = Fixtures.get_file_path("wsdl/SendService.wsdl") |> Wsdl.parse_from_file
-    soap_action = "sendMessage"
-    params      = %{inCommonParms: [{"userID", "WSPB"}]}
     http_poison_result = {:ok, %HTTPoison.Response{status_code: 200, body: "Anything"}}
 
     with_mock(HTTPoison, [post!: fn(_, _, _) -> http_poison_result end]) do
       {_, response} = http_poison_result
-      assert(Soap.call(wsdl, soap_action, params) == {:ok, response.body})
+      assert(Soap.call(wsdl, @soap_action, @request_params) == {:ok, response.body})
     end
   end
 
   test "#call was success, but not found" do
     {_, wsdl}   = Fixtures.get_file_path("wsdl/SendService.wsdl") |> Wsdl.parse_from_file
-    soap_action = "sendMessage"
-    params      = %{inCommonParms: [{"userID", "WSPB"}]}
     http_poison_result = {:ok, %HTTPoison.Response{status_code: 404}}
 
     with_mock(HTTPoison, [post!: fn(_, _, _) -> http_poison_result end]) do
-      assert(Soap.call(wsdl, soap_action, params) == {:error, "Not found"})
+      assert(Soap.call(wsdl, @soap_action, @request_params) == {:error, "Not found"})
     end
   end
 
   test "#call returns error" do
     {_, wsdl}   = Fixtures.get_file_path("wsdl/SendService.wsdl") |> Wsdl.parse_from_file
-    soap_action = "sendMessage"
-    params      = %{inCommonParms: [{"userID", "WSPB"}]}
     http_poison_result = {:error, %HTTPoison.Error{reason: :something_wrong}}
 
     with_mock(HTTPoison, [post!: fn(_, _, _) -> http_poison_result end]) do
-      assert(Soap.call(wsdl, soap_action, params) == {:error, :something_wrong})
+      assert(Soap.call(wsdl, @soap_action, @request_params) == {:error, :something_wrong})
     end
   end
 end
