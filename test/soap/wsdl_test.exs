797c0a88a2040d22968ebaefa4b1f32b936ad875
sigmen <romchky1@gmail.com>
2018-01-15 13:57:13 +0300

    Remove unusual test for private function

--- test/soap/wsdl_test.exs
+++ test/soap/wsdl_test.exs
@@ -79,13 +79,6 @@ defmodule Soap.WsdlTest do
     assert Wsdl.get_complex_types(@wsdl) == types
   end
 
-  test "get_soap_action_by_operation_name returns correctly action" do
-    operation = "SendMessage"
-    soap_action = "com.esendex.ems.soapinterface/SendMessage"
-
-    assert Wsdl.get_soap_action_by_operation_name(@wsdl, operation) == soap_action
-  end
-  
   test "#get_operations returns list of operations" do
     operations = ["SendMessage", "SendMessageMultipleRecipients"]
 
