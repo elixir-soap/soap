70eab8b126fe4bf096f0f4e84ca9570234cceb00
Petr Stepchenko <nitrinodev@gmail.com>
2018-01-17 16:30:01 +0300

    fix tests HTTPoison.post!

--- test/soap_test.exs
+++ test/soap_test.exs
@@ -10,7 +10,7 @@ defmodule SoapTest do
     {_, wsdl} = Fixtures.get_file_path("wsdl/SendService.wsdl") |> Wsdl.parse_from_file
     http_poison_result = {:ok, %HTTPoison.Response{status_code: 200, body: "Anything"}}
 
-    with_mock(HTTPoison, [post!: fn(_, _, _) -> http_poison_result end]) do
+    with_mock(HTTPoison, [post: fn(_, _, _) -> http_poison_result end]) do
       {_, response} = http_poison_result
       assert(Soap.call(wsdl, @operation, @request_params) == {:ok, response.body})
     end
@@ -20,7 +20,7 @@ defmodule SoapTest do
     {_, wsdl} = Fixtures.get_file_path("wsdl/SendService.wsdl") |> Wsdl.parse_from_file
     http_poison_result = {:ok, %HTTPoison.Response{status_code: 404}}
 
-    with_mock(HTTPoison, [post!: fn(_, _, _) -> http_poison_result end]) do
+    with_mock(HTTPoison, [post: fn(_, _, _) -> http_poison_result end]) do
       assert(Soap.call(wsdl, @operation, @request_params) == {:error, "Not found"})
     end
   end
@@ -29,7 +29,7 @@ defmodule SoapTest do
     {_, wsdl} = Fixtures.get_file_path("wsdl/SendService.wsdl") |> Wsdl.parse_from_file
     http_poison_result = {:error, %HTTPoison.Error{reason: :something_wrong}}
 
-    with_mock(HTTPoison, [post!: fn(_, _, _) -> http_poison_result end]) do
+    with_mock(HTTPoison, [post: fn(_, _, _) -> http_poison_result end]) do
       assert(Soap.call(wsdl, @operation, @request_params) == {:error, :something_wrong})
     end
   end
