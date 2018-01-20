0f1a32c4157e5f1ca6d185fc2450de948ceda52a
sigmen <romchky1@gmail.com>
2018-01-20 18:26:58 +0300

    Resolve conflict and added docs to Soap.Request

--- lib/soap/response.ex
+++ lib/soap/response.ex
@@ -1,18 +1,28 @@
 defmodule Soap.Response do
+  @moduledoc """
+  Documentation for Soap.Request.
+  """
   import SweetXml
 
+  @doc """
+  Executing with xml response body and list with tag names.
+  If a list is empty then parse/2 returns full parsed response structure into map.
+  """
+  @spec parse(String.t(), list()) :: map()
   def parse(xml_response, []) do
     xml_response
     |> xpath(~x"//response/*"l)
     |> parse_elements
   end
 
+  @spec parse_record(tuple()) :: map() | String.t()
   defp parse_record({:xmlElement, tag_name, _, _, _, _, _, _, elements, _, _, _}) do
     %{tag_name => parse_elements(elements)}
   end
   defp parse_record({:xmlText, _, _, _, value, _}) when is_list(value), do: List.to_string(value)
   defp parse_record({:xmlText, _, _, _, value, _}) when is_binary(value), do: value
 
+  @spec parse_elements(list() | tuple()) :: map()
   defp parse_elements(elements) when is_list(elements) do
     elements
     |> Enum.map(fn x -> parse_record(x) end)
@@ -20,6 +30,7 @@ defmodule Soap.Response do
   end
   defp parse_elements(elements) when is_tuple(elements), do: parse_record(elements)
 
+  @spec handle_elements_list(list()) :: map()
   defp handle_elements_list(elements) do
     case Enum.all?(elements, fn x -> is_map(x) end) do
     true ->
@@ -29,6 +40,7 @@ defmodule Soap.Response do
     end
   end
 
+  @spec extract_value_from_list(list()) :: any()
   defp extract_value_from_list(elements) do
     if Enum.count(elements) > 1, do: elements, else: List.first(elements)
   end
