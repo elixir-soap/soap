b0ad1ef46e0663d51871d71386fb60dd007166e6
Paul Spieker <p.spieker@duenos.de>
2018-12-19 16:01:50 +0100

    FEAT: Support for SOAP headers

--- lib/soap/request/params.ex
+++ lib/soap/request/params.ex
@@ -16,55 +16,23 @@ defmodule Soap.Request.Params do
   @date_time_type_regex "[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}"
 
   @doc """
-  Headers generator by soap action and custom headers.
-  ## Examples
-
-  """
-  @spec build_headers(map(), String.t(), list()) :: list()
-  def build_headers(wsdl, operation, custom_headers) do
-    wsdl
-    |> extract_soap_action_by_operation(operation)
-    |> extract_headers(custom_headers)
-  end
-
-  @doc """
-  Returns endpoint url from wsdl.
-  """
-  @spec get_url(wsdl :: map()) :: String.t()
-  def get_url(wsdl) do
-    wsdl.endpoint
-  end
-
-  @doc """
   Parsing parameters map and generate body xml by given soap action name and body params(Map).
   Returns xml-like string.
   """
 
-  @spec build_body(wsdl :: map(), operation :: String.t() | atom(), params :: map()) :: String.t()
-  def build_body(wsdl, operation, params) do
-    case params |> construct_xml_request_body |> validate_params(wsdl, operation) do
-      {:error, messages} ->
-        messages
-
-      validated_params ->
-        validated_params
-        |> add_action_tag_wrapper(wsdl, operation)
-        |> add_body_tag_wrapper
-        |> add_envelope_tag_wrapper(wsdl, operation)
-        |> document
-        |> generate(format: :none)
-        |> String.replace(["\n", "\t"], "")
+  @spec build_body(wsdl :: map(), operation :: String.t() | atom(), params :: map(), headers :: map()) :: String.t()
+  def build_body(wsdl, operation, params, headers) do
+    with {:ok, body} <- build_soap_body(wsdl, operation, params),
+         {:ok, header} <- build_soap_header(wsdl, operation, headers) do
+      [header, body]
+      |> add_envelope_tag_wrapper(wsdl, operation)
+      |> document
+      |> generate(format: :none)
+      |> String.replace(["\n", "\t"], "")
+    else
+      {:error, message} -> message
     end
   end
-
-  @spec base_headers(String.t()) :: list()
-  defp base_headers(soap_action) do
-    [{"SOAPAction", soap_action}, {"Content-Type", "text/xml;charset=utf-8"}]
-  end
-
-  @spec extract_headers(String.t(), list()) :: list()
-  defp extract_headers(soap_action, []), do: base_headers(soap_action)
-  defp extract_headers(_, custom_headers), do: custom_headers
 
   @spec validate_params(params :: any(), wsdl :: map(), operation :: String.t()) :: any()
   def validate_params(params, _wsdl, _operation) when is_binary(params), do: params
@@ -139,6 +107,36 @@ defmodule Soap.Request.Params do
 
   def validate_type(k, _v, type = "dateTime"), do: type_error_message(k, type)
 
+  defp build_soap_body(wsdl, operation, params) do
+    case params |> construct_xml_request_body |> validate_params(wsdl, operation) do
+      {:error, messages} ->
+        {:error, messages}
+
+      validated_params ->
+        body =
+          validated_params
+          |> add_action_tag_wrapper(wsdl, operation)
+          |> add_body_tag_wrapper
+
+        {:ok, body}
+    end
+  end
+
+  defp build_soap_header(wsdl, operation, headers) do
+    case headers |> construct_xml_request_header do
+      {:error, messages} ->
+        {:error, messages}
+
+      validated_params ->
+        body =
+          validated_params
+          |> add_header_part_tag_wrapper(wsdl, operation)
+          |> add_header_tag_wrapper
+
+        {:ok, body}
+    end
+  end
+
   defp type_error_message(k, type) do
     "Element #{k} has wrong type. Expects #{type} type."
   end
@@ -165,6 +163,24 @@ defmodule Soap.Request.Params do
   defp construct_xml_request_body(params) when is_atom(params) or is_number(params), do: params |> to_string
   defp construct_xml_request_body(params) when is_binary(params), do: params
 
+  @spec construct_xml_request_header(params :: map() | list()) :: list()
+  defp construct_xml_request_header(params) when is_map(params) or is_list(params) do
+    params |> Enum.map(&construct_xml_request_header/1)
+  end
+
+  @spec construct_xml_request_header(params :: tuple()) :: tuple()
+  defp construct_xml_request_header(params) when is_tuple(params) do
+    params
+    |> Tuple.to_list()
+    |> Enum.map(&construct_xml_request_header/1)
+    |> insert_tag_parameters
+    |> List.to_tuple()
+  end
+
+  @spec construct_xml_request_header(params :: String.t() | atom() | number()) :: String.t()
+  defp construct_xml_request_header(params) when is_atom(params) or is_number(params), do: params |> to_string
+  defp construct_xml_request_header(params) when is_binary(params), do: params
+
   @spec insert_tag_parameters(params :: list()) :: list()
   defp insert_tag_parameters(params) when is_list(params), do: params |> List.insert_at(1, nil)
 
@@ -180,6 +196,19 @@ defmodule Soap.Request.Params do
     [element(action_tag, action_tag_attributes, body)]
   end
 
+  @spec add_header_part_tag_wrapper(list(), map(), String.t()) :: list()
+  defp add_header_part_tag_wrapper(body, wsdl, operation) do
+    action_tag_attributes = handle_element_form_default(wsdl[:schema_attributes])
+
+    case get_header_with_namespace(wsdl, operation) do
+      nil ->
+        nil
+
+      action_tag ->
+        [element(action_tag, action_tag_attributes, body)]
+    end
+  end
+
   defp handle_element_form_default(%{target_namespace: ns, element_form_default: "qualified"}), do: %{xmlns: ns}
   defp handle_element_form_default(_schema_attributes), do: %{}
 
@@ -191,6 +220,24 @@ defmodule Soap.Request.Params do
     wsdl[:complex_types]
     |> Enum.find(fn x -> x[:name] == operation end)
     |> handle_action_extractor_result(wsdl, operation)
+  end
+
+  @spec get_header_with_namespace(wsdl :: map(), operation :: String.t()) :: String.t()
+  defp get_header_with_namespace(wsdl, operation) do
+    with %{input: %{header: %{message: message, part: part}}} <-
+           Enum.find(wsdl[:operations], &(&1[:name] == operation)),
+         %{name: name} <- get_message_part(wsdl, message, part) do
+      name
+    else
+      _ -> nil
+    end
+  end
+
+  defp get_message_part(wsdl, message, part) do
+    wsdl[:messages]
+    |> Enum.find(&("tns:#{&1[:name]}" == message))
+    |> Map.get(:parts)
+    |> Enum.find(&(&1[:name] == part))
   end
 
   defp handle_action_extractor_result(nil, wsdl, operation) do
@@ -210,6 +257,9 @@ defmodule Soap.Request.Params do
 
   @spec add_body_tag_wrapper(list()) :: list()
   defp add_body_tag_wrapper(body), do: [element(:"#{env_namespace()}:Body", nil, body)]
+
+  @spec add_header_tag_wrapper(list()) :: list()
+  defp add_header_tag_wrapper(body), do: [element(:"#{env_namespace()}:Header", nil, body)]
 
   @spec add_envelope_tag_wrapper(body :: any(), wsdl :: map(), operation :: String.t()) :: any()
   defp add_envelope_tag_wrapper(body, wsdl, operation) do
@@ -239,11 +289,6 @@ defmodule Soap.Request.Params do
 
   defp prepare_action_attribute(action_attribute_namespace, action_attribute_value) do
     %{"xmlns:#{action_attribute_namespace}" => action_attribute_value}
-  end
-
-  @spec extract_soap_action_by_operation(map(), String.t()) :: String.t()
-  defp extract_soap_action_by_operation(wsdl, operation) do
-    Enum.find(wsdl[:operations], fn x -> x[:name] == operation end)[:soap_action]
   end
 
   defp soap_version(wsdl) do
