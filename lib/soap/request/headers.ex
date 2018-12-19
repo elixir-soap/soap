dcfe8131c38247ede3c1749bdf1441679855cef1
Paul Spieker <p.spieker@duenos.de>
2018-12-19 16:01:50 +0100

    FEAT: Support for SOAP headers

---
+++ lib/soap/request/headers.ex
@@ -0,0 +1,32 @@
+defmodule Soap.Request.Headers do
+  @doc """
+  Headers generator by soap action and custom headers.
+
+  ## Examples
+
+      iex> {:ok, wsdl} = Fixtures.get_file_path("wsdl/SendService.wsdl") |> Soap.init_model(:file)
+      ...> Soap.Request.Headers.build(wsdl, "SendMessage", [])
+      [{"SOAPAction", "com.esendex.ems.soapinterface/SendMessage"}, {"Content-Type", "text/xml;charset=utf-8"}]
+
+  """
+  @spec build(map(), String.t(), list()) :: list()
+  def build(wsdl, operation, custom_headers) do
+    wsdl
+    |> extract_soap_action_by_operation(operation)
+    |> extract_headers(custom_headers)
+  end
+
+  @spec extract_soap_action_by_operation(map(), String.t()) :: String.t()
+  defp extract_soap_action_by_operation(wsdl, operation) do
+    Enum.find(wsdl[:operations], fn x -> x[:name] == operation end)[:soap_action]
+  end
+
+  @spec extract_headers(String.t(), list()) :: list()
+  defp extract_headers(soap_action, []), do: base_headers(soap_action)
+  defp extract_headers(_, custom_headers), do: custom_headers
+
+  @spec base_headers(String.t()) :: list()
+  defp base_headers(soap_action) do
+    [{"SOAPAction", soap_action}, {"Content-Type", "text/xml;charset=utf-8"}]
+  end
+end
--- test/soap/request_test.exs
+++ test/soap/request_test.exs
@@ -4,6 +4,24 @@ defmodule Soap.RequestTest do
   doctest Soap.Request
   alias Soap.{Wsdl, Request}
 
+  @request_with_header """
+                       <?xml version="1.0" encoding="UTF-8"?>
+                       <env:Envelope xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tns="http://test.com" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                         <env:Header>
+                            <Authentication xmlns="http://test.com">
+                               <token>barbaz</token>
+                            </Authentication>
+                         </env:Header>
+                         <env:Body>
+                           <tns:sayHello xmlns="http://test.com">
+                             <body>Hello John</body>
+                           </tns:sayHello>
+                         </env:Body>
+                       </env:Envelope>
+                       """
+                       |> String.replace(~r/>\n.*?</, "><")
+                       |> String.trim()
+
   test "#call returns response body" do
     {_, wsdl} = Fixtures.get_file_path("wsdl/SendService.wsdl") |> Wsdl.parse_from_file()
     operation = "SendMessage"
@@ -24,6 +42,24 @@ defmodule Soap.RequestTest do
 
     with_mock HTTPoison, post: fn _, _, _, [hackney: ^hackney] -> http_poison_result end do
       assert(Request.call(wsdl, operation, params, [], hackney: hackney) == http_poison_result)
+    end
+  end
+
+  test "#get_url returns correct soap:address" do
+    endpoint = "http://localhost:8080/soap/SendService"
+    {_, wsdl} = Fixtures.get_file_path("wsdl/SendService.wsdl") |> Wsdl.parse_from_file()
+    result = wsdl[:endpoint]
+
+    assert result == endpoint
+  end
+
+  test "#call takes a tuple with soap headers and params" do
+    {_, wsdl} = Fixtures.get_file_path("wsdl/SoapHeader.wsdl") |> Wsdl.parse_from_file()
+    operation = "sayHello"
+    params = {%{token: "barbaz"}, %{body: "Hello John"}}
+
+    with_mock HTTPoison, post: fn _, body, _, _ -> body end do
+      assert(Request.call(wsdl, operation, params) == @request_with_header)
     end
   end
 end
