ded4443e3cdae5c4e8b717167d909a5cd9ddcbf2
Petr Stepchenko <nitrinodev@gmail.com>
2019-02-14 15:00:57 +0700

    Add Soap.Response.parse/1

--- test/soap/response_test.exs
+++ test/soap/response_test.exs
@@ -4,14 +4,26 @@ defmodule Soap.ResponseTest do
   alias Soap.Response
   alias Soap.Response.Parser
 
-  test "#parse response, when request was successful" do
+  test "#Soap.Response.parse/1 from Soap.Response struct, when request was successful" do
+    response = %Response{
+      body: Fixtures.load_xml("send_service/SendMessageResponse.xml"),
+      headers: [],
+      request_url: "",
+      status_code: 200
+    }
+    correctly_parsed_response = %{response: %{message: "Hello!"}}
+
+    assert Response.parse(response) == correctly_parsed_response
+  end
+
+  test "#Soap.Response.parse/2, when request was successful" do
     xml_body = Fixtures.load_xml("send_service/SendMessageResponse.xml")
     correctly_parsed_response = %{response: %{message: "Hello!"}}
 
     assert Response.parse(xml_body, 200) == correctly_parsed_response
   end
 
-  test "#parse response, when response contains fault" do
+  test "#Soap.Response.parse/2, when response contains fault" do
     parsed_fault = %{
       faultcode: "soap:Server",
       faultstring: "System.Web.Services.Protocols.SoapException: Server was unable to process request."
