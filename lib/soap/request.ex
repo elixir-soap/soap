40674704fbe63310c82ff2b0001e88cf72d8c200
Nicol Acosta <nicolacosta8@gmail.com>
2022-07-04 17:14:54 -0500

    removed option from call func, just the env var can force the ssl

--- lib/soap/request.ex
+++ lib/soap/request.ex
@@ -10,19 +10,18 @@ defmodule Soap.Request do
   Calling HTTPoison request by Map with method, url, body, headers, options keys.
   """
   @spec call(wsdl :: map(), operation :: String.t(), params :: any(), headers :: any(), opts :: any()) :: any()
-  def call(wsdl, operation, soap_headers_and_params, https?, request_headers \\ [], opts \\ [])
+  def call(wsdl, operation, soap_headers_and_params, request_headers \\ [], opts \\ [])
 
-  def call(wsdl, operation, {soap_headers, params}, https?, request_headers, opts) do
-    https? = if is_nil(https?), do: force_https?(), else: https?
-    url = get_url(wsdl, https?)
+  def call(wsdl, operation, {soap_headers, params}, request_headers, opts) do
+    url = get_url(wsdl, force_https?())
     request_headers = Headers.build(wsdl, operation, request_headers)
     body = Params.build_body(wsdl, operation, params, soap_headers)
 
     get_http_client().post(url, body, request_headers, opts)
   end
 
-  def call(wsdl, operation, params, https?, request_headers, opts),
-      do: call(wsdl, operation, {%{}, params}, https?, request_headers, opts)
+  def call(wsdl, operation, params, request_headers, opts),
+      do: call(wsdl, operation, {%{}, params}, request_headers, opts)
 
   @spec get_http_client() :: HTTPoison.Base
   def get_http_client do
