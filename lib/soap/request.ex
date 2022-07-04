c025416494ef901836749576b81475a0fdf4aa17
Nicol Acosta <nicolacosta8@gmail.com>
2022-07-04 16:19:52 -0500

    added option to enable force https

--- lib/soap/request.ex
+++ lib/soap/request.ex
@@ -10,27 +10,30 @@ defmodule Soap.Request do
   Calling HTTPoison request by Map with method, url, body, headers, options keys.
   """
   @spec call(wsdl :: map(), operation :: String.t(), params :: any(), headers :: any(), opts :: any()) :: any()
-  def call(wsdl, operation, soap_headers_and_params, request_headers \\ [], opts \\ [])
+  def call(wsdl, operation, soap_headers_and_params, https? \\ force_https?(), request_headers \\ [], opts \\ [])
 
-  def call(wsdl, operation, {soap_headers, params}, request_headers, opts) do
-    url = get_url(wsdl)
+  def call(wsdl, operation, {soap_headers, params}, https?, request_headers, opts) do
+    url = get_url(wsdl, https?)
     request_headers = Headers.build(wsdl, operation, request_headers)
     body = Params.build_body(wsdl, operation, params, soap_headers)
 
     get_http_client().post(url, body, request_headers, opts)
   end
 
-  def call(wsdl, operation, params, request_headers, opts),
-    do: call(wsdl, operation, {%{}, params}, request_headers, opts)
+  def call(wsdl, operation, params, https?, request_headers, opts),
+    do: call(wsdl, operation, {%{}, params}, https?, request_headers, opts)
 
   @spec get_http_client() :: HTTPoison.Base
   def get_http_client do
     Application.get_env(:soap, :globals)[:http_client] || HTTPoison
   end
 
-  @spec get_url(wsdl :: map()) :: String.t()
-  defp get_url(wsdl) do
-    wsdl.endpoint
-    |> String.replace("http://", "https://")
+  @spec force_https?() :: boolean()
+  def force_https? do
+    Application.get_env(:soap, :globals)[:force_https] || false
   end
+
+  @spec get_url(wsdl :: map(), boolean()) :: String.t()
+  defp get_url(wsdl, true), do: String.replace(wsdl.endpoint, "http://", "https://")
+  defp get_url(wsdl, _), do: wsdl.endpoint
 end
