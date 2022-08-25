3c7673c014deafea27beba2f0da53f64ae5b25c4
Nicol Acosta <nicolacosta8@gmail.com>
2022-08-25 10:34:00 -0500

    added debug flag

--- lib/soap/request.ex
+++ lib/soap/request.ex
@@ -2,6 +2,8 @@ defmodule Soap.Request do
   @moduledoc """
   Documentation for Soap.Request.
   """
+  require Logger
+
   alias Soap.Request.{Headers, Params}
 
   @doc """
@@ -10,18 +12,22 @@ defmodule Soap.Request do
   Calling HTTPoison request by Map with method, url, body, headers, options keys.
   """
   @spec call(wsdl :: map(), operation :: String.t(), params :: any(), headers :: any(), opts :: any()) :: any()
-  def call(wsdl, operation, soap_headers_and_params, request_headers \\ [], opts \\ [])
+  def call(wsdl, operation, soap_headers_and_params, request_headers \\ [], opts \\ [], flags \\ [])
 
-  def call(wsdl, operation, {soap_headers, params}, request_headers, opts) do
+  def call(wsdl, operation, {soap_headers, params}, request_headers, opts, flags) do
     url = get_url(wsdl, force_https?())
     request_headers = Headers.build(wsdl, operation, request_headers)
     body = Params.build_body(wsdl, operation, params, soap_headers)
 
+    if flags[:debug] do
+      Logger.debug("soap-call-debug", %{url: url, body: body, request_headers: request_headers, opts: opts})
+    end
+
     get_http_client().post(url, body, request_headers, opts)
   end
 
-  def call(wsdl, operation, params, request_headers, opts),
-      do: call(wsdl, operation, {%{}, params}, request_headers, opts)
+  def call(wsdl, operation, params, request_headers, opts, flags),
+      do: call(wsdl, operation, {%{}, params}, request_headers, opts, flags)
 
   @spec get_http_client() :: HTTPoison.Base
   def get_http_client do
